387f8272b24f11ddd52645839488e5a28c510d94
Adam White <adam@jmr.ca>
2023-12-08 14:58:26 -0500

    Moved stimulus controller out of the layout

---
+++ source/javascripts/controllers/share_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js"
+
+export default class extends Controller {
+  static targets = [ "container" ]
+
+  connect() {
+    const containerEl = this.containerTarget
+    const canonicalLink = document.getElementById('canonical')
+    let link = `${document.location.origin}${document.location.pathname}`
+
+    if(canonicalLink) {
+      link = canonicalLink.href
+    }
+
+    const anchor = this.element.previousElementSibling.id
+    if(anchor) {
+      let anchorLink = `${link}#${anchor}`
+
+      var shareLink = document.createElement('a')
+      shareLink.classList.add("black","no-underline","fw4","bb","b--black")
+      var linkText = document.createTextNode("Link here")
+      shareLink.appendChild(linkText)
+      shareLink.title = "Link directly to this section of the newsletter"
+      shareLink.href = anchorLink
+
+      containerEl.innerHTML = " - "
+      containerEl.appendChild(shareLink)
+    }
+  }
+}
